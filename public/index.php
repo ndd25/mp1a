b8ceae2af49176515635982dd0cc1b504aed5ab9
Nigum Dave <ndd25@njit.edu>
2019-02-09 12:41:54 -0500

    Feature: Using a factory (In Progress)

--- public/index.php
+++ public/index.php
@@ -6,5 +6,74 @@
  * Time: 8:19 PM
  */
 
-echo 'Hello World'
+main::start();
+
+class main{
+
+    static public function start(){
+        $records = csv::getRecords();
+        $table = html::generateTable($records);
+        system::printPage($table);
+    }
+}
+
+class csv{
+    static public function getRecords(){
+
+        $make = 'Lambo';
+        $model = 'Bling Wagon';
+        $car = AutomobileFactory::create($make, $model);
+
+        $records[] = $car;
+
+        print_r($records);
+
+        return $records;
+    }
+}
+
+class html{
+    static public function generateTable($records)
+    {
+        $table = $records;
+        return $table;
+    }
+}
+
+class system{
+    static public function printPage($page){
+        echo $page;
+    }
+}
+
+class Automobile
+{
+    private $vehicleMake;
+    private $vehicleModel;
+
+    public function __construct($make, $model)
+    {
+        $this->vehicleMake = $make;
+        $this->vehicleModel = $model;
+    }
+
+    public function getMakeAndModel()
+    {
+        return $this->vehicleMake . ' ' . $this->vehicleModel;
+    }
+}
+
+class AutomobileFactory
+{
+    public static function create($make, $model)
+    {
+        return new Automobile($make, $model);
+    }
+}
+
+// have the factory create the Automobile object
+$veyron = AutomobileFactory::create('Bugatti', 'Veyron');
+
+print_r($veyron->getMakeAndModel()); // outputs "Bugatti Veyron"
+
 ?>
